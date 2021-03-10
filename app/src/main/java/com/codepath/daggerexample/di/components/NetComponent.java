d3d1af1951ff9e02af045e7bf49ba614cdb64318
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-03-10 23:35:12 +0200

    Initial commit

---
+++ app/src/main/java/com/codepath/daggerexample/di/components/NetComponent.java
@@ -0,0 +1,24 @@
+package com.codepath.daggerexample.di.components;
+
+
+import com.codepath.daggerexample.di.modules.AppModule;
+import com.codepath.daggerexample.di.modules.NetModule;
+
+import android.content.SharedPreferences;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+
+@Singleton
+@Component(modules = {AppModule.class, NetModule.class})
+public interface NetComponent {
+    // downstream components need these exposed
+    Retrofit retrofit();
+
+    OkHttpClient okHttpClient();
+
+    SharedPreferences sharedPreferences();
+}
